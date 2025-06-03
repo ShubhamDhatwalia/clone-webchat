08f5253da807ec87c45fcdea91683bd2184ee490
shubhamdhatwalia1@gmail.com <shubhamdhatwalia1@gmail.com>
2025-06-03 13:13:22 +0530

    working for backend

--- whatsapp-webhook/models/campaign.js
+++ whatsapp-webhook/models/campaign.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 const campaignSchema = new mongoose.Schema({
     campaignName: { type: String, required: true, unique: true },
     whatsappNumber: { type: String, required: true },
-    template: { type: mongoose.Schema.Types.ObjectId, ref: 'Template' },
-    contactList: [{ type: String }], 
+    template: { type: String, required: true },
+    contactList: [{ type: String }],
     createdAt: { type: Date, default: Date.now },
 });
 
