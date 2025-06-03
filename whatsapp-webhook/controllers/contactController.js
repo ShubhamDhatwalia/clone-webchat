0f13314c6674cf4b40de0602d334f5b07c039e57
shubhamdhatwalia1@gmail.com <shubhamdhatwalia1@gmail.com>
2025-06-03 17:07:26 +0530

    working for backend

--- whatsapp-webhook/controllers/contactController.js
+++ whatsapp-webhook/controllers/contactController.js
@@ -34,3 +34,26 @@ export const updateContact = async (req, res) => {
     }
 }
 
+
+export const deleteContact = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const contactData = await Contacts.findByIdAndDelete(id);
+        res.json(contactData);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+export const addTags = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { tags } = req.body;
+        const contactData = await Contacts.findByIdAndUpdate(id, { $push: { tags } });
+        res.json(contactData);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
--- whatsapp-webhook/routes/contactRoutes.js
+++ whatsapp-webhook/routes/contactRoutes.js
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 const router = Router();
 
-import { createContact } from '../controllers/contactController';
+import { createContact, fetchContacts, updateContact, deleteContact, addTags } from '../controllers/contactController';
 
 
-router.get('/contacts')
-router.post('/addContact', createContact);
-router.put('/updateContact')
-router.delete('/deleteContact')
-router.patch('/contacts/${id}/tags')
+router.get('/contacts', fetchContacts)
+router.post('/addContacts', createContact);
+router.put('/updateContact', updateContact)
+router.delete('/deleteContact', deleteContact)
+router.patch('/contacts/${id}/tags', addTags)
 router.patch('/contacts/${id}/tags/remove')
 router.patch('/contacts/${id}/notes')
 router.patch('/contacts/${id}/notes/remove')
