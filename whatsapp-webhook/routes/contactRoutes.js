de10ef2e908d146e6f244b60c9758ec709be807c
shubhamdhatwalia1@gmail.com <shubhamdhatwalia1@gmail.com>
2025-06-04 10:29:43 +0530

    working on motes and tags backend

--- whatsapp-webhook/routes/contactRoutes.js
+++ whatsapp-webhook/routes/contactRoutes.js
@@ -1,18 +1,18 @@
 import { Router } from 'express';
 const router = Router();
 
-import { createContact, fetchContacts, updateContact, deleteContact, addTags, removeTags } from '../controllers/contactController.js';
+import { createContact, fetchContacts, updateContact, deleteContact, addTags, removeTags, addNotes, updateNote, removeNotes } from '../controllers/contactController.js';
 
 
 router.get('/contacts', fetchContacts)
-router.post('/addContacts', createContact);
+router.post('/addContacts', createContact)
 router.put('/updateContact/:id', updateContact)
 router.delete('/deleteContact/:id', deleteContact)
 router.patch('/contacts/:id/addTags', addTags)
 router.patch('/contacts/:id/tags/remove', removeTags)
-router.patch('/contacts/:id/addNotes')
-router.patch('/contacts/:id/notes/remove')
-router.patch('/contacts/:id/notes/update')
+router.patch('/contacts/:id/addNotes', addNotes)
+router.patch('/contacts/:id/notes/remove', removeNotes)
+router.patch('/contacts/:id/notes/update', updateNote)
 
 
 
