80eea315a85c192745c26d5c113636ad72f2ae5f
shubhamdhatwalia1@gmail.com <shubhamdhatwalia1@gmail.com>
2025-06-11 12:36:48 +0530

    working on socket.io

--- whatsapp-webhook/controllers/chatController.js
+++ whatsapp-webhook/controllers/chatController.js
@@ -3,10 +3,14 @@ import chat from "../models/chat.js";
 
 export const fetchChat = async (req, res) => {
     try {
-        const phone = req.params.phone;
-        console.log(phone);
-        const chat = await chat.find({ phone: phone });
-        res.json(chat);
+        const rawPhone = req.params.phone;
+        const phone = rawPhone.replace(/^\+/, '');
+
+        console.log('Sanitized phone:', phone);
+
+        const chats = await chat.find({ phone: phone });
+        res.json(chats);
+
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: err.message });
