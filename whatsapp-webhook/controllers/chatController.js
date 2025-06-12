9e81e08774089174855766fea870358d59a77aac
shubhamdhatwalia1@gmail.com <shubhamdhatwalia1@gmail.com>
2025-06-12 17:04:07 +0530

    working on socket io for media messages

--- whatsapp-webhook/controllers/chatController.js
+++ whatsapp-webhook/controllers/chatController.js
@@ -1,6 +1,5 @@
 import chat from "../models/chat.js";
 
-
 export const fetchChat = async (req, res) => {
     try {
         const rawPhone = req.params.phone;
@@ -8,30 +7,34 @@ export const fetchChat = async (req, res) => {
 
         console.log('Sanitized phone:', phone);
 
-        const chats = await chat.find({ "message.from": phone });
-        res.json(chats);
+        const chats = await chat
+            .find({ "message.from": phone })
+            .sort({ "message.timestamp": 1 })
+            .lean(); // boost performance
 
+        res.json(chats);
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: err.message });
-
     }
-}
+};
+
+
 export const fetchAllChats = async (req, res) => {
     try {
         const chats = await chat.aggregate([
-            { $sort: { "message.timestamp": -1 } }, 
+            { $sort: { "message.timestamp": -1 } },
             {
                 $group: {
-                    _id: "$message.from",         
-                    latestChat: { $first: "$$ROOT" } 
+                    _id: "$message.from",
+                    latestChat: { $first: "$$ROOT" }
                 }
             },
             {
-                $replaceRoot: { newRoot: "$latestChat" } 
+                $replaceRoot: { newRoot: "$latestChat" }
             },
             {
-                $sort: { "message.timestamp": -1 } 
+                $sort: { "message.timestamp": -1 }
             }
         ]);
 
